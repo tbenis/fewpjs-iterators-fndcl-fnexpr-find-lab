522cf8a91f78a29fdcb42084ea324658202851d5
Benis Tambe <will.jenifer@yahoo.com>
2021-08-13 14:00:31 -0400

    commit tree d9377e26910f29e012bb9629582f82fc2e9f280d

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+function superbowlWin(record /* arr */){
+    // arr.find(r => {
+    //     r.result === "W";
+    //     return r.year;
+    // })
+    // return undefined;
+
+    /*
+    Sol */
+    let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
+    
+}
